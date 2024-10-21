3d02ad45700a3751c624a438d23b178deb170d15
TaufiqueUmar <taufique.231451.it@mhssce.ac.in>
2024-10-21 22:18:13 +0530

    Add files via upload

---
+++ LL.java
@@ -0,0 +1,114 @@
+class LL {
+    Node head;
+
+    class Node {
+        String data;
+        Node next;
+
+        Node(String data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // add first
+    public void addFirst(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // add last
+
+    public void addLast(String data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node currNode = head;
+        while (currNode.next != null) {
+            currNode = currNode.next;
+        }
+        currNode.next = newNode;
+    }
+
+    // print list same as add last
+    public void printList() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+        Node currNode = head;
+        while (currNode != null) {
+            System.out.print(currNode.data + "->");
+            currNode = currNode.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public void deleteFirst(){
+        if(head==null){
+            System.out.println("LIST IS EMPTY");
+        }
+
+        head = head.next; 
+   }
+
+   public void deleteLast(){
+        if(head==null){
+            System.out.println("List is empty");
+        }
+        if(head.next == null){
+            head = null;
+        }
+
+        Node secondLast = head;
+        Node LastNode = head.next; //head.next = null -> 
+        while(LastNode.next !=null ){ //LastNOde.next = null.next(error)
+            LastNode = LastNode.next;
+            secondLast = secondLast.next;
+        }
+        secondLast.next = null;
+    }
+
+    public Node reverse (Node head){
+        Node curr = head;
+        Node prev = null;
+        while(curr !=null){
+            Node temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.addFirst("a");
+        list.addFirst("is");
+        list.printList();
+
+        list.addLast("List");
+        list.printList();
+
+        list.addFirst("This");
+        list.printList();
+
+        // list.deleteFirst();
+        // list.printList();
+
+        // list.deleteLast();
+        // list.printList();
+
+        list.reverse("This");
+        list.printList();
+    }
+}
